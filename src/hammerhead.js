1870f2e83abfa2c5618ada00d31fc2c339934e62
Daniel McKeown <2019@sf3am.com>
2020-01-22 12:28:52 -0800

    add header meta data and inject component into element instead of document.body

--- src/hammerhead.js
+++ src/hammerhead.js
@@ -1,5 +1,4 @@
 import { h, Component, render } from '/web_modules/preact.js';
-//  import { html } from '/web_modules/htm/preact.js';
 import htm from '/web_modules/htm.js';
 
 const html = htm.bind(h);
@@ -14,6 +13,16 @@ class Hammerhead extends Component {
           <br />
           <footer>(utilizes <a href="https://github.com/developit/htm">htm</a> and <a href="https://preactjs.com/">Preact</a> for components)</footer>
           <style>
+            :root {
+              --uiFonts: "Ubuntu Mono", "Inconsolata", "Anonymous Pro", "Hack", Menlo,
+                monospace;
+              --contentFonts: "Lato", "Open Sans", "Lucida Grande", "Ubuntu Sans",
+                "Segoe UI", "Roboto", Helvetica, sans-serif;
+              --displayFonts: "Gentona", "Baufra", Georgia, serif;
+              --monoFonts: "Anonymous Pro", "Hack", "Fira Sans", "Inconsolata", monospace;
+              --textFonts: "Calluna", "Caslon", "Garamond" serif;
+              --sFonts: Courier, "Open Sans", monospace;
+            }
             aside {
               border-style: groove;
               border-color: aqua;
@@ -23,7 +32,7 @@ class Hammerhead extends Component {
               width: 98vw;
             }
             h3, footer {
-              font-family: Courier, "Open Sans", monospace;
+              font-family: var(--sFonts, sans);
             }
           </style>
         </div>
@@ -31,4 +40,4 @@ class Hammerhead extends Component {
   }
 }
 
-render(html`<${Hammerhead} />`, document.body);
+render(html`<${Hammerhead} />`, document.getElementById('main'));
