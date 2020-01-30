aea26751699342ed74243ebef510b4f5d7591a23
Daniel McKeown <2019@sf3am.com>
2020-01-30 13:27:09 -0800

    update default hammerhead to use grid for layout

--- src/hammerhead.js
+++ src/hammerhead.js
@@ -8,8 +8,14 @@ class Hammerhead extends Component {
     return html`
         <div id="main">
           <h3>Henlo Hammerhead Shark</h3>
-          <img src="../static/img/jonas-allert-v4xMAiisCx0-unsplash.jpg" alt="hammerhead-shark" class="pic" />
-          <footer id="web-component-boilerplate">web-component-boilerplate from <a href="https://danmckeown.info">Dan McKeown</a> </footer>
+          <div class="grid">
+            <section id="sidebar">µ</section>
+            <img src="../static/img/jonas-allert-v4xMAiisCx0-unsplash.jpg" alt="hammerhead-shark" class="pic" />
+            <footer id="web-component-boilerplate">web-component-boilerplate from <a href="https://danmckeown.info">Dan McKeown</a> </footer>
+            <aside id="rightSide">∑ß</aside>
+          </div>
+          
+          
           <br />
           <footer>(utilizes <a href="https://github.com/developit/htm">htm</a> and <a href="https://preactjs.com/">Preact</a> for components)</footer>
           <style>
@@ -23,16 +29,24 @@ class Hammerhead extends Component {
               --textFonts: "Calluna", "Caslon", "Garamond" serif;
               --sFonts: Courier, "Open Sans", monospace;
             }
-            aside {
+            aside, footer#web-component-boilerplate {
               border-style: groove;
               border-color: aqua;
               border-width: 1pt;
             }
-            img {
-              width: 98vw;
+            img.pic {
+              width: 100%;
+              height: auto;
             }
             h3, footer {
               font-family: var(--sFonts, sans);
+            }
+            .grid {
+              display: grid;
+              grid-gap: 10px;
+              grid-template-columns: 100px 1fr 3fr 1fr;
+              background-color: #fff;
+              color: #444;
             }
           </style>
         </div>
